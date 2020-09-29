85cb1b612534f482bba5a5934c524f7dc864450a
jacob-ok <jacob-ok@qq.com>
2020-09-29 14:00:38 +0800

    2020-09-29 14:00 second commit

--- commons-io/src/main/java/com/tmszh/MainApp.java
+++ commons-io/src/main/java/com/tmszh/MainApp.java
@@ -13,21 +13,21 @@ import java.io.IOException;
  */
 public class MainApp {
     public static void main(String[] args) {
-        File file = new File("D:\\tmszh\\document\\readme.txt");
+        File file = new File("D:\\thomas-ok\\document\\readme.txt");
         //创建一个文件夹，如果由于某些原因导致不能创建，则抛出异常
         //一次可以创建单级或者多级目录
         try {
-            FileUtils.forceMkdir(new File("D:\\tmszh\\Downloads"));
+            FileUtils.forceMkdir(new File("D:\\thomas-ok\\Downloads"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("D:\\tmszh\\Downloads 目录创建成功!");
+        System.out.println("D:\\thomas-ok\\Downloads 目录创建成功!");
         //为指定文件创建文件的父级目录
         try {
             FileUtils.forceMkdirParent(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("D:\\tmszh\\document\\readme.txt 父级目录创建成功");
+        System.out.println("D:\\thomas\\document\\readme.txt 父级目录创建成功");
     }
 }
