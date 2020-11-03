b00a847d407a995700efa308b645ee12395cc193
thomas-ok <thomas-ok@qq.com>
2020-11-04 07:31:53 +0800

    2020-11-4 second commit

---
+++ multiThreading/traverseFolder/src/main/java/org/thomas/FolderTraversal.java
@@ -0,0 +1,20 @@
+package org.thomas;
+
+import java.io.File;
+
+/**
+ * author Andrew
+ * version V1.0
+ * 2020/11/4 7:27
+ * Description:
+ */
+public class FolderTraversal {
+    public static void main(String[] args) {
+
+    }
+
+    public static void traverse(File dir){
+
+    }
+
+}
